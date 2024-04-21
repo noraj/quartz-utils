839aaa2abb263825f9e520efc42206c3e96546c6
noraj <alexandre.zanni@europe.com>
2024-04-22 01:16:21 +0200

    release ci fix

--- compile_x86_64-linux.cr
+++ compile_x86_64-linux.cr
@@ -1,6 +1,6 @@
 require "yaml"
 
-platforms = %w[x86_64-linux-gnu]
+platforms = %w[x86_64-linux-gnu x86_64-linux-musl]
 
 shard = {} of YAML::Any => YAML::Any
 File.open("shard.yml") do |file|
@@ -11,7 +11,7 @@ executables = shard["executables"].as_a
 executables.each do |executable|
   platforms.each do |platform|
     puts "[+] #{executable}-#{platform}"
-    cc = `docker run --rm -it -v $PWD:/workspace -w /workspace crystallang/crystal:1.12.1-alpine crystal build src/#{executable}.cr --release --static --no-debug -o bin/#{executable}-#{platform} --cross-compile --target #{platform}`
+    cc = `crystal build src/#{executable}.cr --release --static --no-debug -o bin/#{executable}-#{platform} --cross-compile --target #{platform}`
     system(cc)
   end
 end
